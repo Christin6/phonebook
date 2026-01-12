59336d1e2b448abc64d84cef68b29f358a6ae738
Christin6 <94294443+Christin6@users.noreply.github.com>
2026-01-12 22:11:53 +0700

    change use order

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ let persons = [
     }
 ]
 
-app.use(express.json())
-
 app.use(express.static('dist'))
+
+app.use(express.json())
 
 morgan.token('data-sent', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data-sent'))
